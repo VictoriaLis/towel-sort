d67a1cd18c805a4f69700da0caac7fd22ee568ad
Victoria Lis <victorialis001@gmail.com>
2020-10-25 18:53:07 +0300

    feat: create towel-sort func

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-
 // You should implement your task here.
+const _ = require('lodash');
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined) {
+        return [];
+    }
+    return _.flatten(matrix.map((element, index) => (index % 2 !== 0) ? element.reverse() : element));
 }
+
